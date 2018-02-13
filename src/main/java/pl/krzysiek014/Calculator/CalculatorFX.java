d9868cf8d37468ec885796951558350a9a1a603c
Krzysiek014 <krzysiek014@o2.pl>
2018-02-14 00:16:02 +0100

    Added Application functionality

--- src/main/java/pl/krzysiek014/Calculator/CalculatorFX.java
+++ src/main/java/pl/krzysiek014/Calculator/CalculatorFX.java
@@ -1,10 +1,17 @@
 package pl.krzysiek014.Calculator;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Created by Krzysiek014 on 13.02.2018.
  */
-public class CalculatorFX {
+public class CalculatorFX extends Application{
     public static void main(String[] args){
+        launch(args);
+    }
 
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
     }
 }
