b74aebc278b1fb56ef688c0a7f6c81828a538ad5
Krzysiek014 <krzysiek014@o2.pl>
2018-02-14 15:25:50 +0100

    Added functions for number buttons and CE functionality

--- src/main/java/pl/krzysiek014/Calculator/Controller.java
+++ src/main/java/pl/krzysiek014/Calculator/Controller.java
@@ -12,10 +12,57 @@ public class Controller{
     @FXML
     Button one,two,three,four,five,six, seven, eight, nine, zero;
     @FXML
-    Button multiple, add, subtract, divide, percent, sqrt, plusMin, equals, dot;
+    Button multiple, add, subtract, divide, percent, sqrt, plusMin, equals, dot, ce;
     @FXML
     TextField lcd;
 
+    String operator = null;
 
+//    Button[] listOfNumbers = new Button[]{one,two,three,four,five,six, seven, eight, nine, zero};
+    public void initialize(){
+        // Numbers functionality
+        one.setOnMouseClicked(e->{
+            check(1);
+        });
+        two.setOnMouseClicked(e->{
+            check(2);
+        });
+        three.setOnMouseClicked(e->{
+            check(3);
+        });
+        four.setOnMouseClicked(e->{
+            check(4);
+        });
+        five.setOnMouseClicked(e->{
+            check(5);
+        });
+        six.setOnMouseClicked(e->{
+            check(6);
+        });
+        seven.setOnMouseClicked(e->{
+            check(7);
+        });
+        eight.setOnMouseClicked(e->{
+            check(8);
+        });
+        nine.setOnMouseClicked(e->{
+            check(9);
+        });
+        zero.setOnMouseClicked(e->{
+            check(0);
+        });
 
+        // Calculator functions
+        ce.setOnMouseClicked(e->{
+            lcd.setText("0");
+        });
+    }
+
+    private void check(int num){
+        if(operator!=null){
+
+        }else{
+            lcd.setText(Integer.valueOf(lcd.getText()+num)+"");
+        }
+    }
 }
